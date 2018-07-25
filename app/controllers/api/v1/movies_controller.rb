2418f0f51afd0157be8d46e97800155904119bd9
spreered <spreered@gmail.com>
2018-07-25 16:06:20 +0800

    add movies/show and movie.id

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -4,6 +4,7 @@ class Api::V1::MoviesController < ApiController
     render json:{
       results: @movies.map do |movie|
         {
+          id: movie.id,
           title: movie.title,
           genres: movie.genres,
           description: movie.description,
@@ -14,5 +15,16 @@ class Api::V1::MoviesController < ApiController
     }
   end
   def show
+    @movie = Movie.find_by(id: params[:id])
+    render json:{
+      results: {
+          id: @movie.id,
+          title: @movie.title,
+          genres: @movie.genres,
+          description: @movie.description,
+          release_date: @movie.release_date,
+          image: @movie.image
+      }
+    }
   end
 end
