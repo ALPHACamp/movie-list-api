548a8c66e38695a5286f4eb42418571d384a740f
wells <jhang0912407249@gmail.com>
2022-05-29 18:42:30 +0800

    try to fix attach error

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,11 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+require "active_storage/attached"
+ActiveSupport.on_load(:active_record) do
+  extend ActiveStorage::Attached::Macros
+end
+
 module MovieList
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
